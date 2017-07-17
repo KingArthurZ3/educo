c80daaf9ab30acc5255cdf138ed451a95027eb11
Arthur Zhang <arthur@Arthurs-MacBook-Pro.local>
2017-07-16 22:38:39 -0700

    added scroll feature

---
+++ indexScript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    document.getElementById("home").onclick = function(){
+        $("html, body").animate({scrollTop: "250px"});
+    }
+
+    document.getElementById("aboutEdu").onclick = function(){
+        $("html, body").animate({scrollTop: "1000px"});
+    }
+
+
+})
